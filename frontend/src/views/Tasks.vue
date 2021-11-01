7672b1998da8c696bbf321a925ec8c89ffe94952
elias4k <40409522+elias4k@users.noreply.github.com>
2021-11-01 13:05:57 -0300

    Install Vue, Vuetify, Router

---
+++ frontend/src/views/Tasks.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-card
+    class="mx-auto mt-6"
+    max-width="344"
+  >
+    <v-card-text>
+      <div>Word of the Day</div>
+      <p class="text-h4 text--primary">
+        Title
+      </p>
+      <div class="text--primary">
+        Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, asperiores quisquam distinctio delectus quo odit harum placeat tenetur suscipit laboriosam facilis. Earum, voluptate accusantium. Esse consequatur officia et hic similique!
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="teal accent-4"
+        @click="reveal = true"
+      >
+        Eliminar
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <v-card
+        v-if="reveal"
+        class="transition-fast-in-fast-out v-card--reveal"
+        style="height: 100%;"
+      >
+        <v-card-text class="pb-0">
+          <p class="text-h4 text--primary">
+            Origin
+          </p>
+          <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
+        </v-card-text>
+        <v-card-actions class="pt-0">
+          <v-btn
+            text
+            color="teal accent-4"
+            @click="reveal = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+
+
+<style>
+.v-card--reveal {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
+}
+</style>
+
+<script>
+  export default {
+    data: () => ({
+      reveal: false,
+    }),
+  }
+</script>
